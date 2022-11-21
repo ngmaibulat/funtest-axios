61f6a806a896e77aa50fe2f993e07b5564872e16
ngm <ngmaibulat@gmail.com>
2022-11-21 17:35:53 +0300

    minor

--- src/github.test.js
+++ src/github.test.js
@@ -65,6 +65,34 @@ const http = axios.create({
     },
 });
 
+// http.interceptors.request.use(
+//     function (config) {
+//         // Do something before request is sent
+//         console.log(config.baseURL + config.url);
+//         return config;
+//     },
+//     function (error) {
+//         // Do something with request error
+//         return Promise.reject(error);
+//     }
+// );
+
+// http.interceptors.response.use(
+//     function (response) {
+//         // Any status code that lie within the range of 2xx cause this function to trigger
+//         // Do something with response data
+//         console.log(response.headers);
+//         return response;
+//     },
+//     function (error) {
+//         // Any status codes that falls outside the range of 2xx cause this function to trigger
+//         // Do something with response error
+//         return Promise.reject(error);
+//     }
+// );
+
+// console.log(http.interceptors.request.handlers);
+
 beforeAll(async () => {
     samples = await import(samplesPath);
 });
